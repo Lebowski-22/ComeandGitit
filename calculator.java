879dc92e2a1eedcdb9c9dda027b207a4cfb3145b
Kenneth Castro <cken51213@gmail.com>
2021-11-24 13:13:55 +0800

    Added multiplication and division feature. Created a new file.

--- calculator.java
+++ calculator.java
@@ -4,7 +4,7 @@ class calculator {
 
     public static void main(String[] args) {
 
-        int firstnum, secondnum, total, totalDiff;
+        int firstnum, secondnum, totalSum, totalDiff, totalProd, totalQuotient;
 
         // get user input
         Scanner scanner = new Scanner(System.in);
@@ -14,12 +14,17 @@ class calculator {
 
         System.out.println("Enter second number: ");
         secondnum = scanner.nextInt();
+        scanner.close();
 
-        total = firstnum + secondnum;
+        totalSum = firstnum + secondnum;
         totalDiff = firstnum - secondnum;
+        totalProd = firstnum * secondnum;
+        totalQuotient = firstnum / secondnum;
 
-        System.out.println("Sum of the two numbers: " + total);
+        System.out.println("Sum of the two numbers: " + totalSum);
         System.out.println("Difference of the two numbers: " + totalDiff);
+        System.out.println("Product of the two numbers: " + totalProd);
+        System.out.println("Quotient of the two numbers: " + totalQuotient);
 
     }
 }
